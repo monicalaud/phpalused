af8fabfa94eabdd9fdbb7d1c5a47b91bcfad5826
Monica Laud <monica.laud@khk.ee>
2016-10-17 11:09:10 -0400

    echo kasutamine

---
+++ praks1/ylesanne_1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<title>
+	<?php echo 'PHP ylessanne 1';?>
+	</title>
+	<meta charset='utf-8'/>
+	</head>
+	<body> 
+	
+		  <h1>PHP katsetus</h1>
+<?php
+echo 'Kell on: '.date('H:i:s'); //see väljastab veebi kella
+?>
+<?php
+echo '<h2>Monica Laud</h2>';
+echo '<p>Monica Laud <br> IT nooremspetsialist <br> IS115  <br><br><b>monica.laud@khk.ee</p>'; # kasutasin ridade eralduseks br
+?> 
+<?php
+echo "<font color='red'>Selle kursuse valisin,kuna minu jaoks on programeerimine ja sellega seotu huvitav</font>";
+?>
+<br>
+<br>
+<?php
+echo "<font color='Palevioletred'>Tahaks kuskil natuke rohkem seletust leida. Millal kasutada ühekordeid jutumärke ja millal kahekordseid??</font>";
+?>
+	</body>
+</html>   
