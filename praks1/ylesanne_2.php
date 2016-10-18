d81c1ba1f5a42c7e4dcc571e0b3403f23873551f
Monica Laud <monica.laud@khk.ee>
2016-10-18 01:41:27 -0400

    praks2 pole vist päris valmis?? võisobib see nii ka??

---
+++ praks1/ylesanne_2.php
@@ -0,0 +1,48 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>ylesanne 2</title>
+			<meta charset='utf-8'/>
+	
+	<style>
+	body { background-image: url("http://www.planwallpaper.com/static/images/518164-backgrounds.jpg");
+}
+</style>
+</head>
+	<body> 
+		<h1>ylesanne 2</h2><br>
+		<h3>Monica Laud IS115</h3>
+			<p style="text-align:center" > <strong>Tere tulemast  siia !</strong><br>
+Ülesandes kasutatud muutujad ja väärtused:
+<ol>
+<li>$a = "Mari"</li>
+<li>$b = 6</li>
+<li>$c = -2</li>
+<li>$korrutis = $b * $c</li>
+<li>$e = True</li>
+<li>$f =3.2</li>
+</ol>
+			</p>
+	
+<?php 
+'<br>'; 
+$d= "tehe";
+$a = "Mari";'<br>';
+$b = 6;'<br>';
+$c = -2;'<br>';
+$korrutis = $b * $c;
+$e = True;
+$f = 3.2;'<br>';
+echo " $a arvutab: $d $b korda $c on võrdne $korrutis".'<br>';'<br>';
+
+'<br>';
+echo gettype($a).'<br>';  
+echo gettype($b).'<br>';  
+echo gettype ($c).'<br>';  
+echo gettype($korrutis).'<br>';  
+echo gettype($d).'<br>';  
+echo gettype($e).'<br>'; 
+ echo gettype($f).'<br>';
+?>  
+	</body>
+</html>   
